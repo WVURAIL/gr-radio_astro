bb4c796be3b3fcef6a5a643425bff985f5a17eb4
Glen Langston <glen.i.langston@gmail.com>
2018-05-04 20:52:15 -0400

    separate vave and vmedian

--- python/ra_vmedian.py
+++ python/ra_vmedian.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2018 Glen Langston.
+# Copyright 2018 <+YOU OR YOUR COMPANY+>.
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
