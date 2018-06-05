5850641d618c2bbfc485cd7bb8b8ace173e6837d
Kevin Bandura <kbandura@gmail.com>
2018-06-05 12:52:09 -0400

    added support to hdf5 sink for selectable number of inputs and data type

--- python/hdf5_sink.py
+++ python/hdf5_sink.py
@@ -28,12 +28,21 @@ class hdf5_sink(gr.sync_block):
     """
     docstring for block hdf5_sink
     """
-    def __init__(self, vec_length, fname='default.h5', pointing = "AZ,EL", freq_start=1419.0, freq_step=0.002 , notes = 'default' ):
+    def __init__(self, intype, n_inputs, vec_length, fname='default.h5', pointing = "AZ,EL", freq_start=1419.0, freq_step=0.002 , notes = 'default' ):
         current_time = time.time()
+        if intype == complex:
+            datatype = np.complex64
+        elif intype == float:
+            datatype = np.float32
+        elif intype == int:
+            datatype = np.int32
+        else:
+            raise
         gr.sync_block.__init__(self,
             name="hdf5_sink",
-            in_sig=[(np.complex64, vec_length)],
+            in_sig=n_inputs*[(datatype, vec_length)],
             out_sig=None)
+        print(intype)
         self.h5 = h5py.File(fname, 'w')
         self.h5.attrs["file_name"] = fname
         self.h5.attrs["notes"] = notes
@@ -42,30 +51,37 @@ class hdf5_sink(gr.sync_block):
         self.h5.attrs['freq_start'] = freq_start
         self.h5.attrs["freq_step"] = freq_step
         self.timeDataset = self.h5.create_dataset('timestamp', (1,1), dtype=np.float64, maxshape=(None,1))
-        self.spectrumDataset = self.h5.create_dataset('spectrum', (1,vec_length), dtype=np.complex64, maxshape=(None,vec_length))
+        self.inputDataset = self.h5.create_dataset('input', (1,1), dtype=np.float64, maxshape=(None,1))
+        self.spectrumDataset = self.h5.create_dataset('spectrum', (1,n_inputs*vec_length), dtype=datatype, maxshape=(None,vec_length))
         self.n_times = 1
         self.n = 0
         self.vec_size = vec_length
+        self.n_inputs = n_inputs
 
 
 
     def work(self, input_items, output_items):
-        in0 = input_items[0]
+        #makes a kind of 'flat' file instead of a matrix for inputs.  
+        #all inputs must be same datatype.
         current_time = time.time()
-        #oversized = in0.size/self.vec_size
-        #print(in0.shape)
-        ### Seems can actually get a bunch of vectors as the input piled up.
-        #this doesn't seem to break if only have one.
-        for inp0 in in0:
-            if self.n == self.n_times:
-                self.n_times = self.n+1
-                self.timeDataset.resize((self.n_times,1))
-                self.spectrumDataset.resize((self.n_times,self.vec_size))
-            else:
-                pass
-            print( self.n_times) #debugging.  remove
-            self.timeDataset[self.n] = current_time
-            self.spectrumDataset[self.n] = inp0
-            self.n += 1
+        for in_num, in0 in enumerate(input_items):
+            #in0 = input_items[0]
+            #oversized = in0.size/self.vec_size
+            #print(in0.shape)
+            ### Seems can actually get a bunch of vectors as the input piled up.
+            #this doesn't seem to break if only have one.
+            for inp0 in in0:
+                if self.n == self.n_times:
+                    self.n_times = self.n+1
+                    self.timeDataset.resize((self.n_times,1))
+                    self.inputDataset.resize((self.n_times, 1))
+                    self.spectrumDataset.resize((self.n_times,self.vec_size))
+                else:
+                    pass
+                print( self.n_times) #debugging.  remove
+                self.timeDataset[self.n] = current_time
+                self.inputDataset[self.n] = in_num
+                self.spectrumDataset[self.n] = inp0
+                self.n += 1
         return len(input_items[0])
 
